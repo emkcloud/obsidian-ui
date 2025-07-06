95c35e651b0c02488020c8f6f394db51eba1fc6e
Massimo <massimo@emarket.pe>
2025-07-06 08:40:13 +0200

    Update service provider view namespace

--- src/Core/PackageManager.php
+++ src/Core/PackageManager.php
@@ -13,13 +13,13 @@ class PackageManager
 
     public function classesIcon($size)
     {
-        return $this->classes('shrink-0')->add(match($size)
+        return $this->classes('shrink-0')->add(match ($size)
         {
-            'large'    => '[:where(&)]:size-8 [:where(&)]:stroke-[1.5]',
+            'large' => '[:where(&)]:size-8 [:where(&)]:stroke-[1.5]',
             'standard' => '[:where(&)]:size-6 [:where(&)]:stroke-[2.0]',
-            'small'    => '[:where(&)]:size-5 [:where(&)]:stroke-[2.4]',
-            'micro'    => '[:where(&)]:size-4 [:where(&)]:stroke-[3.0]',
-            default    => '[:where(&)]:size-6 [:where(&)]:stroke-[2.0]',
+            'small' => '[:where(&)]:size-5 [:where(&)]:stroke-[2.4]',
+            'micro' => '[:where(&)]:size-4 [:where(&)]:stroke-[3.0]',
+            default => '[:where(&)]:size-6 [:where(&)]:stroke-[2.0]',
         });
     }
 }
